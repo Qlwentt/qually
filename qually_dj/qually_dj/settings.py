75d8451b2a54f63075cf54b2f32b84c29d955604
Qlwentt <qlwentt@gmail.com>
2017-01-06 08:20:50 -0800

    creates job app and assoc. views/templates

--- qually_dj/qually_dj/settings.py
+++ qually_dj/qually_dj/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage',
+    'jobs',
+    # 'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- qually_dj/qually_dj/urls.py
+++ qually_dj/qually_dj/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('homepage.urls')),
+    url(r'^jobs/', include('jobs.urls')),
 ]
