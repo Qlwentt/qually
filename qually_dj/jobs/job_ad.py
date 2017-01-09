8de4d3ceac4a1a76627aa0fc1e345917d1a7656d
Qlwentt <qlwentt@gmail.com>
2017-01-09 15:07:22 -0800

    adds quick and dirty tests to the bottom of JobAd class

--- qually_dj/jobs/job_ad.py
+++ qually_dj/jobs/job_ad.py
@@ -2,6 +2,10 @@ from __future__ import unicode_literals
 import urllib2
 from textblob import TextBlob as tb
 from BeautifulSoup import BeautifulSoup
+import re
+import requests
+
+
 
 class JobAd(object):
 	"""" A job ad from the Indeed API. It has the following properties:
@@ -17,28 +21,31 @@ class JobAd(object):
 		date: date posted;			datetime
 	"""
 
-	@staticmethod
-	def fix_spacing(text):
-		  return re.sub(r'([a-z:.)])([A-Z])', r'\1 \2', text)
-
 
 	def __init__(self, properties):
 		#Returns job ad object with properties
 		self.key=properties['key']
-		self.title=properties.title['title']
-		self.snippet=properties.snippet['snippet']
+		self.title=properties['title']
+		self.snippet=properties['snippet']
 		self.content=''
-		self.url=properties.url['url']
-		self.company=properties.url['company']
-		self.location=properties.url['location']
-		self.date=properties.date['date']
+		self.url=properties['url']
+		self.company=properties['company']
+		self.location=properties['location']
+		self.date=properties['date']
+
+
+	@staticmethod
+	def fix_spacing(text):
+		  return re.sub(r'([a-z:.)])([A-Z])', r'\1 \2', text)
 
 	@classmethod
 	def make_job_ads_from_api(cls,api_json_resp):
 		job_ads=[]
-
+			
 		for result in api_json_resp:
-			job_ads.append(cls.JobAd({
+			# print "result: {}".format(result)
+			# print "result type: {}".format(type(result[0]))
+			job_ads.append(JobAd({
 				'key': result['jobkey'],
 				'title': result['jobtitle'],
 				'snippet': result['snippet'],
@@ -47,17 +54,26 @@ class JobAd(object):
 				'location': result['formattedLocation'],
 				'date': result['date']
 				})) 
+		for job_ad in job_ads:
+			job_ad.content=job_ad.set_content()
+
 		return job_ads
 	
 	def set_content(self):	
+	    # self.content=job_ad_text
 	    html = urllib2.urlopen(self.url).read()
 	    soup = BeautifulSoup(html)
 	    job_ad_text = soup.find('span', attrs={'id': 'job_summary'}).text
-	    job_ad_text=fix_spacing(job_ad_text)
-	  	self.content=job_ad_text
-		return job_ad_text
-
-	
+	    job_ad_text= JobAd.fix_spacing(job_ad_text)
+	    return job_ad_text
 
 
+indeed_request_url="http://api.indeed.com/ads/apisearch?publisher=9253729351823762&q=software engineer&l=seattle%2C+wa&sort=&radius=&st=&jt=&start={}&limit=1000&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+APIresponse = requests.get(indeed_request_url).json()['results']
+test_job_ads=JobAd.make_job_ads_from_api(APIresponse)
+# print test_job_ads
+# print len(test_job_ads)
+# print type(test_job_ads)
+# print type(test_job_ads[0])
+print dir(test_job_ads[0])
 
