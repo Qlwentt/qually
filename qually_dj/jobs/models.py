d34b43727980caf96067cf51fa9eff096ffc866a
Qlwentt <qlwentt@gmail.com>
2017-01-11 08:51:42 -0800

    creates SavedJob model

--- qually_dj/jobs/models.py
+++ qually_dj/jobs/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class SavedJob(models.Model):
+	title = models.CharField()
+	url = models.CharField()
+	snippet = models.TextField()
+	key = models.CharField()
+	date = models.DateTimeField()
+	company = models.CharField()
+	location = models.CharField()
+
+	def __unicode__(self):
+		return self.title
