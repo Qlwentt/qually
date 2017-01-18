6841d068a17766fd5e8a2d72b18666108529c3c2
Qlwentt <qlwentt@gmail.com>
2017-01-18 08:37:17 -0800

    adds stuff to WSGI in attempt to fix production problems

--- qually_dj/qually_dj/wsgi.py
+++ qually_dj/qually_dj/wsgi.py
@@ -12,11 +12,15 @@ import os
 # from dotenv import load_dotenv, find_dotenv
 # load_dotenv(find_dotenv())
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qually_dj.settings")
-
-application = get_wsgi_application()
+# add the hellodjango project path into the sys.path
+sys.path.append('/Users/quaiwentt/Desktop/Ada/capstone/qually/qually_dj')
 
 # add the conda_env site-packages path to the sys.path
 sys.path.append('/Users/quaiwentt/anaconda/envs/qually_env/lib/python2.7/site-packages')
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qually_dj.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
