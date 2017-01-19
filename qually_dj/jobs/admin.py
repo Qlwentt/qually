beb81697d482c6fc6ad7b16f260f5c37078c6503
Qlwentt <qlwentt@gmail.com>
2017-01-19 13:55:45 -0800

    continue hacky stuff

--- qually_dj/jobs/admin.py
+++ qually_dj/jobs/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from jobs.models import Keyword
-from jobs.models import SavedJob
-from jobs.models import Resume
+try:
+    
+	from jobs.models import Keyword
+	from jobs.models import SavedJob
+	from jobs.models import Resume
+except ImportError:
+	from qually_dj.jobs.models import Keyword
+	from qually_dj.jobs.models import SavedJob
+	from qually_dj.jobs.models import Resume
+
+
 # Register your models here.
 admin.site.register(Keyword)
 admin.site.register(SavedJob)
