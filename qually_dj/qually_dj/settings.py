4ddb4fbb6e3a196a9150d1cd26164c3d14925728
Qlwentt <qlwentt@gmail.com>
2017-01-19 13:41:28 -0800

    import sys

--- qually_dj/qually_dj/settings.py
+++ qually_dj/qually_dj/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
+import os,sys
 from os.path import join, dirname
 # from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
