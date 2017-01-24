c2aa05391c95b3392c9c891840ac20c91b2af514
Qlwentt <qlwentt@gmail.com>
2017-01-24 14:44:42 -0800

    fixes deployment problems

--- homepage/management/commands/createsu.py
+++ homepage/management/commands/createsu.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(username="qlwentt").exists():
             User.objects.create_superuser(os.environ["ADMIN_USER_NAME"], os.environ["ADMIN_EMAIL"], os.environ["ADMIN_PASSWORD"])
