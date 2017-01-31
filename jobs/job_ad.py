9776985bef385d89fa41432c29d39658e407e18b
Qlwentt <qlwentt@gmail.com>
2017-01-30 20:03:21 -0800

    fixes syntax error

--- jobs/job_ad.py
+++ jobs/job_ad.py
@@ -112,7 +112,7 @@ class JobAd(object):
 	    answer = None
 	    # numbers=["one","two","three","four","five","six","seven","eight","nine","ten","1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 	    get_digits={"one": 1, "1" : 1,"two": 2, "2": 2, "three": 3, "3": 3, "four": 4, "4": 4, "five": 5,"5": 5, "six": 6,"6": 6, "seven": 7,"7": 7,"eight": 8,"8": 8,"nine": 9,"9": 9,"ten": 10,"10": 10}
-	    found_match = re.match(r'(\d)(\s|\W|\d)', inputString))
+	    found_match = re.match(r'(\d)(\s|\W|\d)', inputString).group(0)
 	    for number in get_digits:
 	    	words = inputString.split()
 	    	for word in words:
