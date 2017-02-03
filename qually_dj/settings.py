a15e9a710bfa8028972eb016d9c2bfc5288f468f
Qlwentt <qlwentt@gmail.com>
2017-02-03 12:58:33 -0800

    heroku

--- qually_dj/settings.py
+++ qually_dj/settings.py
@@ -15,6 +15,7 @@ from os.path import join, dirname
 # from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 print "BASE_DIR", BASE_DIR
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 print "PROJECT_ROOT", PROJECT_ROOT
