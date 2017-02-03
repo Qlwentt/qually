a4e1c207351b64d332e4abcff60db590e9578564
Qlwentt <qlwentt@gmail.com>
2017-02-03 12:16:33 -0800

    heroku

--- qually_dj/settings.py
+++ qually_dj/settings.py
@@ -184,7 +184,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,"homepage", "static"), os.path.join(BASE_DIR,"homepage", "static", "homepage"),
+    os.path.join(BASE_DIR, "static"),
     # os.path.join(BASE_DIR,"homepage", "static", "fonts"), os.path.join(BASE_DIR,"homepage", "static", "js")
 )
 
