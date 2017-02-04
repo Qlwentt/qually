9bb7c2f2cc856436a56c582ab2357b9612370735
Qlwentt <qlwentt@gmail.com>
2017-02-03 17:40:57 -0800

    installs whitenoise to try to fix static files

--- qually_dj/settings.py
+++ qually_dj/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
