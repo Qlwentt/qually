6059155e382d4ffe75a6f8926ec7f9f0f7ab10be
Qlwentt <qlwentt@gmail.com>
2017-02-04 17:31:51 -0800

    heroku

--- qually_dj/settings.py
+++ qually_dj/settings.py
@@ -59,13 +59,13 @@ INSTALLED_APPS = [
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
-        'djangosecure',
+        # 'djangosecure',
 ]
 ROOT_URLCONF = 'qually_dj.urls'
 
 MIDDLEWARE = [
-    'djangosecure.middleware.SecurityMiddleware',
-    # 'django.middleware.security.SecurityMiddleware',
+    # 'djangosecure.middleware.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
