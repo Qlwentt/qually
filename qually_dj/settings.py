5f71ff7e40a7fbbe6733d68c33554dc4c8d5dd20
Qlwentt <qlwentt@gmail.com>
2017-02-04 18:41:13 -0800

    adds dj-database-url for heroku database config

--- qually_dj/settings.py
+++ qually_dj/settings.py
@@ -125,8 +125,10 @@ try:
                 'PORT': '',
             }
         } 
-except IndexError:    
-    print "using production database"
+except IndexError:
+    import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
