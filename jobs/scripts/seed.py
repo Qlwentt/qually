397e0a052057a838af42f404d6e4304ffef3ab6a
Qlwentt <v-quaiw@zillowgroup.com>
2017-05-08 21:37:30 -0700

    fixes bugs in seed script

--- jobs/scripts/seed.py
+++ jobs/scripts/seed.py
@@ -1,21 +1,23 @@
-# Full path and name to your csv file
-csv_filepathname="/Users/quaiw/portfolio/kw-scraper/keywords.csv"
-# Full path to the directory immediately above your django project directory
-your_djangoproject_home="/Users/quaiw/portfolio/"
-
 import sys,os
-sys.path.append(your_djangoproject_home)
+from os.path import join, dirname
+# Full path and name to your csv file
+csv_filepathname= os.path.join(BASE_DIR, "keywords.csv")
+# Full path to the directory immediately above your django project directory
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(PROJECT_ROOT)
 os.environ['DJANGO_SETTINGS_MODULE'] ='qually.settings'
 
 from qually.jobs.models import Keword
 
 import csv
 
-dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
+def run():
+	dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
 
-for row in dataReader:
-    keyword=Keyword()
-    keyword.name=row[0]
-    keyword.category=row[1]
+	for row in dataReader:
+	    if row[0] != '' && row[0] != " "
+		    keyword=Keyword()
+		    keyword.name=row[0]
+		    keyword.category=row[1]
 
-    keyword.save()
+		    keyword.save()
