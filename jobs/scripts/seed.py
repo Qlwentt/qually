e4283888794795fab568151effd7670f8c475506
Qlwentt <v-quaiw@zillowgroup.com>
2017-05-08 22:16:15 -0700

    syntax error

--- jobs/scripts/seed.py
+++ jobs/scripts/seed.py
@@ -1,9 +1,10 @@
 import sys,os
 from os.path import join, dirname
 # Full path and name to your csv file
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 csv_filepathname= os.path.join(BASE_DIR, "keywords.csv")
 # Full path to the directory immediately above your django project directory
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(PROJECT_ROOT)
 os.environ['DJANGO_SETTINGS_MODULE'] ='qually.settings'
 
